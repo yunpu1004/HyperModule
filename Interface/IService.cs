37327c981542c1bfeb9f53e3efca36e0eaf21d40
yunpu1004 <53960432+yunpu1004@users.noreply.github.com>
2025-10-01 14:17:13 +0900

    인터페이스 갱신

--- Interface/IService.cs
+++ Interface/IService.cs
@@ -1,26 +1,26 @@
 namespace HyperModule
 {
-    public interface IExecute
+    public interface IService
     {
         void Execute();
     }
 
-    public interface IExecute<T>
+    public interface IService<T>
     {
         void Execute(T data);
     }
 
-    public interface IExecute<T1, T2>
+    public interface IService<T1, T2>
     {
         void Execute(T1 data1, T2 data2);
     }
 
-    public interface IExecute<T1, T2, T3>
+    public interface IService<T1, T2, T3>
     {
         void Execute(T1 data1, T2 data2, T3 data3);
     }
 
-    public interface IExecute<T1, T2, T3, T4>
+    public interface IService<T1, T2, T3, T4>
     {
         void Execute(T1 data1, T2 data2, T3 data3, T4 data4);
     }
